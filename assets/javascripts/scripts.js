0a5b980ec181e40fd07f2b3f1ebf260e6ac4b74f
Alex Davidovic <alexandar87@gmail.com>
2015-04-13 13:38:04 +0200

    Screenshot

--- assets/javascripts/scripts.js
+++ assets/javascripts/scripts.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+jQuery(document).ready(function($) {
 
   $('select, input[type=text], input[type=email], input[type=password], textarea').addClass('form-control');
   $('input[type=submit]').addClass('btn btn-primary');
